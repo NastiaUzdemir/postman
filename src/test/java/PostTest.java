ef7f08e2540864e0649a04d935c8304db4c83469
Neto test <uzdemir.a@yandex.ru>
2024-03-17 14:08:27 +0300

    first commit

---
+++ src/test/java/PostTest.java
@@ -0,0 +1,24 @@
+import io.restassured.RestAssured;
+import jdk.jfr.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostTest {
+
+    @Test
+    public void testPostmanEcho() {
+        RestAssured.baseURI = "https://postman-echo.com";
+
+        given()
+                .body("some data")
+        .when()
+                .post("/post")
+        .then()
+                .statusCode(200)
+                //.body("data", equalTo("wrong data"));
+                .body("data", equalTo("some data"));
+
+    }
+}
